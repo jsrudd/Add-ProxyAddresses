2262dc48bf4d1d2d9c4a5ff187e41c74753c9f75
Optoisolated <46586989+optoisolated@users.noreply.github.com>
2019-09-17 16:21:01 +1000

    Added user exists validation.

--- Add-ProxyAddress.ps1
+++ Add-ProxyAddress.ps1
@@ -6,29 +6,33 @@ Param(
 Import-Module ActiveDirectory
 $Matched=$False
 $ADUser = Get-ADUser -Identity $Username -Properties SamAccountName, ProxyAddresses, Mail
-"Found User $($ADUser.Name)"
-$ProxyAddresses = $ADUser.ProxyAddresses
-$Mail = $ADUser.Mail
-$Mail
-$NewAddress = $EmailAddress.ToLower().Trim()
+if ($ADUser -ne $Null) {
+	"Found User $($ADUser.Name)"
+	$ProxyAddresses = $ADUser.ProxyAddresses
+	$Mail = $ADUser.Mail
+	$Mail
+	$NewAddress = $EmailAddress.ToLower().Trim()
 
-Foreach ($Address in $ProxyAddresses) {
-	$CheckAddress = $Address.Split(":")[1].ToLower()
-    "Checking $CheckAddress for $NewAddress"
-	If ($CheckAddress -eq $NewAddress) {
-		$Matched = $True
-		Break
-	}
-} 
+	Foreach ($Address in $ProxyAddresses) {
+		$CheckAddress = $Address.Split(":")[1].ToLower()
+    	"Checking $CheckAddress for $NewAddress"
+		If ($CheckAddress -eq $NewAddress) {
+			$Matched = $True
+			Break
+		}
+	} 
 
-If ($Matched) {
-	"Alias already present. No action required."
-	} Else {
-	If ($ProxyAddresses.Count -eq 0) {
-		"No aliases present, adding Primary Alias ($Mail)"
-		Set-ADUser -Identity $Username -Add @{Proxyaddresses="SMTP:" + $Mail}
+	If ($Matched) {
+		"Alias already present. No action required."
+		} Else {
+		If ($ProxyAddresses.Count -eq 0) {
+			"No aliases present, adding Primary Alias ($Mail)"
+			Set-ADUser -Identity $Username -Add @{Proxyaddresses="SMTP:" + $Mail}
+		}
+		"Alias not found. Adding ($NewAddress)..."
+		Set-ADUser -Identity $Username -Add @{Proxyaddresses="smtp:" + $NewAddress}
 	}
-	"Alias not found. Adding ($NewAddress)..."
-	Set-ADUser -Identity $Username -Add @{Proxyaddresses="smtp:" + $NewAddress}
+} Else {
+	"User is not found in Active Directory. Exiting"
 }
 ""
